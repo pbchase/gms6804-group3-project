8441719cb13f3c91b2323a6d8268facdf1149028
Philip Chase <philipbchase@gmail.com>
2017-02-26 18:44:10 -0500

    Adjust logging, use temp space for archive extractions, test text file subtypes, and make dummy files where useful

--- make-file-types-consistent.py
+++ make-file-types-consistent.py
@@ -38,6 +38,14 @@ def extract_one_member_from_zip(zf, member, output_dir, path_to_output_file, inp
     return("wrote %s from zip" % path_to_output_file)
 
 
+def write_dummy_file(path_to_output_file):
+    """
+    write an empty file as a place holder
+    """
+    with open(path_to_output_file, 'w') as inF:
+        inF.write("")
+
+
 def extract_zip_to_txt(input_file, path_to_input_file, path_to_output_file, output_dir):
     """
     :param path_to_input_file: the zip file we will read
@@ -50,13 +58,13 @@ def extract_zip_to_txt(input_file, path_to_input_file, path_to_output_file, outp
         zf = zipfile.ZipFile(path_to_input_file, 'r')
     except zipfile.BadZipfile:
         mylog.log([input_file, "archive_content", "archive_is_bad"])
+        write_dummy_file(path_to_output_file)
         return "%s is a bad zip file. Skipping file" % path_to_input_file
     zip_info_list = zf.infolist()
     mylog.log([input_file, "archive_member_count", len(zip_info_list)])
     # extract file from one-member zip files
     if len(zip_info_list) == 1:
         member = zip_info_list[0]
-        mylog.log([input_file, "archive_content", "single_member"])
         if re.search('^genome.*\.txt', member.filename):
             mylog.log([input_file, "archive_content", "name_is_genome_mumble_txt"])
         return(extract_one_member_from_zip(zf, member, output_dir, path_to_output_file, input_file))
@@ -76,6 +84,7 @@ def extract_zip_to_txt(input_file, path_to_input_file, path_to_output_file, outp
             mylog.log([input_file, "archive_content", "archive_has_macosx_artifacts"])
             return(extract_one_member_from_zip(zf, member, output_dir, path_to_output_file, input_file))
 
+    write_dummy_file(path_to_output_file)
     mylog.log([input_file, "archive_content", "unsure_what_to_do_with_content"])
     return("unsure what to do with zip file")
 
@@ -84,9 +93,27 @@ def is_adna_file(input_file):
     """
     Search for an Ancestry DNA header line in input_file
     """
+    return(grep_file_for_header(input_file, header_line='#AncestryDNA raw data download'))
+
+
+def is_23andme_file(input_file):
+    """
+    Search for a 23andMe header line in input_file
+    """
+    return(grep_file_for_header(input_file, header_line='23andme.com/'))
+
+
+def is_vcf_file(input_file):
+    """
+    Search for a VCF header line in input_file
+    """
+    return(grep_file_for_header(input_file, header_line='##fileformat=VCFv'))
+
+
+def grep_file_for_header(input_file, header_line):
     with open(input_file, 'r') as inF:
         for line in inF:
-            if '#AncestryDNA raw data download' in line:
+            if header_line in line:
                 return True
     return False
 
@@ -111,44 +138,57 @@ def extract_gzip_to_txt(path_to_input_file, path_to_output_file, output_dir):
     return("wrote %s from gzip" % path_to_output_file)
 
 
-def convert_raw_input_to_txt_or_vcf(input_dir, txt_dir, vcf_dir, adna_dir):
+def sort_text_file_by_subtype(input_file, input_file_path, txt_dir, vcf_dir, adna_dir, uk_dir):
+    mylog.log([input_file, "download_type", "text"])
+    if is_adna_file(input_file_path):
+        print copy_ascii_to_output(input_file, input_file_path, adna_dir)
+        mylog.log([input_file, "text_file_subtype", "AncestryDNA"])
+    elif is_23andme_file(input_file_path):
+        print copy_ascii_to_output(input_file, input_file_path, txt_dir)
+        mylog.log([input_file, "text_file_subtype", "23andMe"])
+    elif is_vcf_file(input_file_path):
+        print copy_ascii_to_output(input_file, input_file_path, vcf_dir)
+        mylog.log([input_file, "text_file_subtype", "vcf"])
+    else:
+        print copy_ascii_to_output(input_file, input_file_path, uk_dir)
+        mylog.log([input_file, "text_file_subtype", "unknown"])
+
+
+def input_needs_processing(input_file_path, output_file_path, vcf_file_path, adna_file_path, uk_file_path):
+    if not child_younger_than_parent(input_file_path, output_file_path) \
+        and not child_younger_than_parent(input_file_path, vcf_file_path) \
+        and not child_younger_than_parent(input_file_path, adna_file_path) \
+        and not child_younger_than_parent(input_file_path, uk_file_path):
+        return True
+    else:
+        return False
+
+
+def convert_raw_input_to_txt_or_vcf(input_dir, txt_dir, vcf_dir, adna_dir, uk_dir, temp_dir):
     input_files = os.listdir(input_dir)
     for input_file in input_files:
         input_file_path = os.path.join(input_dir, input_file)
         output_file_path = os.path.join(txt_dir, input_file + ".txt")
+        temp_archive_output_file_path = os.path.join(temp_dir, input_file)
         vcf_file_path = os.path.join(vcf_dir, input_file + ".vcf")
         adna_file_path = os.path.join(adna_dir, input_file + ".txt")
-        if not child_younger_than_parent(input_file_path, output_file_path) \
-                and not child_younger_than_parent(input_file_path, vcf_file_path) \
-                and not child_younger_than_parent(input_file_path, adna_file_path):
+        uk_file_path = os.path.join(uk_dir, input_file + ".txt")
+        if input_needs_processing(input_file_path, output_file_path, vcf_file_path, adna_file_path, uk_file_path):
             file_format = magic.from_file(input_file_path)
             mylog.log([input_file, "actual_file_type_from_magic_number", file_format])
             print "Processing %s" % input_file_path ,
             if re.search("^Zip archive data, at least", file_format):
-                print extract_zip_to_txt(input_file, input_file_path, output_file_path, txt_dir)
+                print extract_zip_to_txt(input_file, input_file_path, temp_archive_output_file_path, temp_dir)
                 mylog.log([input_file, "download_type", "archive"])
+                mylog.log([input_file, "member_type_from_magic_number", magic.from_file(temp_archive_output_file_path)])
+                sort_text_file_by_subtype(input_file, temp_archive_output_file_path, txt_dir, vcf_dir, adna_dir, uk_dir)
             elif file_format == "ASCII text, with CRLF line terminators":
-                if is_adna_file(input_file_path):
-                    print copy_ascii_to_output(input_file, input_file_path, adna_dir)
-                    mylog.log([input_file, "download_type", "text"])
-                    mylog.log([input_file, "text_file_subtype", "AncestryDNA"])
-                else:
-                    print copy_ascii_to_output(input_file, input_file_path, txt_dir)
-                    mylog.log([input_file, "download_type", "text"])
-                    mylog.log([input_file, "text_file_subtype", "23andMe"])
+                sort_text_file_by_subtype(input_file, input_file_path, txt_dir, vcf_dir, adna_dir, uk_dir)
             elif re.search("^gzip compressed data", file_format):
-                print extract_gzip_to_txt(input_file_path, output_file_path, txt_dir)
+                print extract_gzip_to_txt(input_file_path, temp_archive_output_file_path, temp_dir)
                 mylog.log([input_file, "download_type", "archive"])
-                magic_number = magic.from_file(output_file_path)
-                mylog.log([input_file, "member_type_from_magic_number", file_format])
-                print "file type of %s is %s" % (output_file_path, magic_number)
-                if not re.search('ASCII (English |)text, with CRLF line terminators', magic.from_file(output_file_path)):
-                    os.rename(output_file_path,vcf_file_path)
-                    mylog.log([input_file, "text_file_subtype", "vcf"])
-                    print "moving %s to %s" % (output_file_path,vcf_file_path)
-                else:
-                    mylog.log([input_file, "text_file_subtype", "23andMe"])
-
+                mylog.log([input_file, "actual_file_type_from_magic_number", magic.from_file(temp_archive_output_file_path)])
+                sort_text_file_by_subtype(input_file, temp_archive_output_file_path, txt_dir, vcf_dir, adna_dir, uk_dir)
             elif re.search("^Variant Call Format", file_format):
                 os.rename(input_file_path,vcf_file_path)
                 print "moving %s to %s" % (input_file_path,vcf_file_path)
@@ -182,11 +222,13 @@ input_dir = "23andmedata/raw"
 txt_dir = "23andmedata/txt"
 vcf_dir = "23andmedata/vcf"
 adna_txt = "adna/txt"
+unknown_txt = "23andmedata/unknown"
+temp_dir = "temp"
 
 conversion_log = "conversion_log.csv"
 mylog = Log(conversion_log)
 
-convert_raw_input_to_txt_or_vcf(input_dir, txt_dir, vcf_dir, adna_txt)
+convert_raw_input_to_txt_or_vcf(input_dir, txt_dir, vcf_dir, adna_txt, unknown_txt, temp_dir)
 
 
 compressed_and_indexed_vcf_dir = "23andmedata/unified"
