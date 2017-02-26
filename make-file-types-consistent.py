0f6d752e24f373c122e2ac8bea61a01b573e7e15
Philip Chase <philipbchase@gmail.com>
2017-02-26 11:44:26 -0500

    Separate AncestryDNA text files from 23andme and make convert_23andme_to_vcf

--- make-file-types-consistent.py
+++ make-file-types-consistent.py
@@ -63,10 +63,21 @@ def extract_zip_to_txt(path_to_input_file, path_to_output_file, output_dir):
     return("unsure what to do with zip file")
 
 
+def is_adna_file(input_file):
+    """
+    Search for an Ancestry DNA header line in input_file
+    """
+    with open(input_file, 'r') as inF:
+        for line in inF:
+            if '#AncestryDNA raw data download' in line:
+                return True
+    return False
+
+
 def copy_ascii_to_output(my_file, my_path, output_path):
     new_name = (os.path.join(output_path, my_file + ".txt"))
     shutil.copy(my_path, new_name)
-    return("wrote %s from ascii" % new_name)
+    return("wrote %s from ascii to %s" % (new_name, output_path))
 
 
 def extract_gzip_to_txt(path_to_input_file, path_to_output_file, output_dir):
@@ -83,20 +94,25 @@ def extract_gzip_to_txt(path_to_input_file, path_to_output_file, output_dir):
     return("wrote %s from gzip" % path_to_output_file)
 
 
-def convert_raw_input_to_txt_or_vcf(input_dir, txt_dir, vcf_dir):
+def convert_raw_input_to_txt_or_vcf(input_dir, txt_dir, vcf_dir, adna_dir):
     input_files = os.listdir(input_dir)
     for input_file in input_files:
         input_file_path = os.path.join(input_dir, input_file)
         output_file_path = os.path.join(txt_dir, input_file + ".txt")
         vcf_file_path = os.path.join(vcf_dir, input_file + ".vcf")
+        adna_file_path = os.path.join(adna_dir, input_file + ".txt")
         if not child_younger_than_parent(input_file_path, output_file_path) \
-                and not child_younger_than_parent(input_file_path, vcf_file_path):
+                and not child_younger_than_parent(input_file_path, vcf_file_path) \
+                and not child_younger_than_parent(input_file_path, adna_file_path):
             file_format = magic.from_file(input_file_path)
             print "Processing %s" % input_file_path ,
             if re.search("^Zip archive data, at least", file_format):
                 print extract_zip_to_txt(input_file_path, output_file_path, txt_dir)
             elif file_format == "ASCII text, with CRLF line terminators":
-                print copy_ascii_to_output(input_file, input_file_path, txt_dir)
+                if is_adna_file(input_file_path):
+                    print copy_ascii_to_output(input_file, input_file_path, adna_dir)
+                else:
+                    print copy_ascii_to_output(input_file, input_file_path, txt_dir)
             elif re.search("^gzip compressed data", file_format):
                 print extract_gzip_to_txt(input_file_path, output_file_path, txt_dir)
                 magic_number = magic.from_file(output_file_path)
@@ -111,7 +127,37 @@ def convert_raw_input_to_txt_or_vcf(input_dir, txt_dir, vcf_dir):
                 print "%s is %s" % (input_file_path, file_format)
 
 
+def convert_23andme_to_vcf(filename, input_dir, output_dir):
+    print ' '.join([filename, input_dir, output_dir])
+    input_file = os.path.join(input_dir, filename)
+    base_name_of_file = re.match('(.*).txt', filename).group(1)
+    name_of_vcf_file = os.path.join(output_dir, base_name_of_file + ".vcf")
+    name_of_compressed_vcf_file = os.path.join(output_dir, base_name_of_file + ".vcf.gz")
+    name_of_vcf_index_file = os.path.join(output_dir, base_name_of_file + ".vcf.gz")
+    if not child_younger_than_parent(input_file, name_of_compressed_vcf_file) \
+            and not child_younger_than_parent(name_of_compressed_vcf_file, name_of_vcf_index_file):
+        output_file_parameter_for_plink = os.path.join(output_dir, base_name_of_file)
+        os.system("./plink --23file %s --snps-only just-acgt --recode vcf --out %s" %
+            (input_file, output_file_parameter_for_plink))
+        os.system("cat %s | bgzip > %s" %
+            (name_of_vcf_file, name_of_compressed_vcf_file))
+        os.unlink(name_of_vcf_file)
+        os.system("tabix -f -p vcf %s" % name_of_compressed_vcf_file)
+
+
 input_dir = "23andmedata/raw"
 txt_dir = "23andmedata/txt"
 vcf_dir = "23andmedata/vcf"
-convert_raw_input_to_txt_or_vcf(input_dir, txt_dir, vcf_dir)
+adna_txt = "adna/txt"
+convert_raw_input_to_txt_or_vcf(input_dir, txt_dir, vcf_dir, adna_txt)
+
+
+compressed_and_indexed_vcf_dir = "23andmedata/unified"
+# convert everything to 23andMe text files
+# convert every 23andme text file to compress and indexed vcf
+
+input_files = os.listdir(txt_dir)
+for filename in input_files:
+    if re.search('.txt$', filename):
+        convert_23andme_to_vcf(filename, txt_dir, compressed_and_indexed_vcf_dir)
+
